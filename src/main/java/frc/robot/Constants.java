3c33bc6615d2ea6220d7654f0c042413e58aeef5
Jonathan Sandjong <jonathan.sandjong83@frenship.net>
2022-10-19 17:30:29 -0500

    Set robot gains to 0 for now

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,11 +3,11 @@ package frc.robot;
 public class Constants {
     public static final int kTimeoutMs = 10;
     // TODO: Find optimal F, P, I, and D gains for robot.
-    public static final double kF = 10;
-    public static final double kP = 10;
-    public static final double kI = 10;
-    public static final double kD = 10;
-    public static final double kIntegralZone = 50;
+    public static final double kF = 0;
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double kIntegralZone = 0;
     public static final double kMaxSteerPercent = 0.25;
     public static final int kSteerLoopPeriodMs = 50;
 }
